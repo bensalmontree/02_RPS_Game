fcb998ba68ae955d4741bdf05ed5f75037b74a7d
bensalmontree <78379397+bensalmontree@users.noreply.github.com>
2021-05-07 13:20:22 +1200

    Update Base_Component

--- 01_RPS_Base_Component.py
+++ 01_RPS_Base_Component.py
@@ -51,6 +51,36 @@ def choice_checker(question, valid_list, error):
         print(error)
         print()
 
+# Displays instuctions if asked
+def instructions():
+    print("**** How to Play ****\n")
+    print("Choose either a number of rounds or press <enter> for infinite mode\n")
+    print("Then for each round, choose from rock / paper / scissors (or xxx to quit)")
+    print("You can type r / p / s / x if you don't want to type the entire word.\n")
+    print("The rules are...")
+    print("- Rock beats scissor")
+    print("- Scissor beats paper")
+    print("Paper beats rock\n")
+    print("*** Have fun ***\n")
+    return ""
+
+# Confirms yes / no response
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes / no")
+
 
 # Main routine goes here
 
@@ -60,7 +90,11 @@ rps_list = ["rock", "paper", "scissors", "xxx"]
 
 # Ask user if they have played before
 # If 'yes', show instructions
+played_before = yes_no("Have you played the "
+                           "game before? ")
 
+if played_before == "no":
+    instructions()
 
 # Ask user for # of rounds then loop...
 game_summary = []
@@ -78,7 +112,11 @@ while end_game == "no":
     # Start of Game Play Loop
 
     # Rounds Heading
-    print()
+    
+    # End game if exit code is typed
+    if user_choice == "xxx":
+        break    print()
+        
     if rounds == "":
         heading = "Continuous Mode: Rounds {}".format(rounds_played + 1)
     else:
@@ -109,29 +147,21 @@ while end_game == "no":
         rounds_lost += 1
 
     if result == "tie":
-        feedback = "It's a tie"
+        feedback = "Round: {}: {} vs {} - It's a tie".format(rounds_played, user_choice, comp_choice)
     else:
         feedback = "{} vs {} - You {}".format(user_choice, comp_choice, result)
 
-    # End game if exit code is typed
-    if user_choice == "xxx":
-        break
 
-    # **** rest of loop / game ****
 
     # Output W/L statements and results
     print(feedback)
-   
-
     rounds_played += 1
 
     # End game if requested # of rounds has been played
     if rounds_played == rounds:
         break
 
-# Ask user if they want to see their game history
-# If 'yes' show game history
-
+# Calculate game history
     if result == "lost" or result == "won":
         outcome = "Round: {}: {} vs {} - You {}".format(rounds_played, user_choice, comp_choice, result)
     else:
@@ -147,16 +177,20 @@ percent_win = rounds_won / rounds_played * 100
 percent_lose = rounds_lost / rounds_played * 100
 percent_tie = rounds_drawn / rounds_played * 100
 
+# Ask user if they want to see their game history
 print()
-print("**** Game History****")
-for game in game_summary:
-    print(game)
+game_history = yes_no("Do you want to see game history? ")
 
-print()
+if game_history == "yes":
+    print()
+    print("**** Game History ****\n")
+    for game in game_summary:
+        print(game)
 
 # Displays game state with % values to the nearest whole number
+print()
 print("******* Game Statistics *******")
-print("Win: {}, ({:.0f}%)\nLoss: {}, ({:.0f}%)\nTie: {}, ({:.0f}%:)".format(rounds_won, percent_win, rounds_lost, percent_lose, rounds_drawn, percent_tie))
+print("Win: {}, ({:.0f}%)\nLoss: {}, ({:.0f}%)\nTie: {}, ({:.0f}%:)\n".format(rounds_won, percent_win, rounds_lost, percent_lose, rounds_drawn, percent_tie))
 
 # End of Game Statements
 print()
